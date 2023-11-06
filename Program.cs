9e8d9465b0b597159c72349ebd5b2b563b83f704
renderivera <31817478+renderivera@users.noreply.github.com>
2023-11-06 14:39:18 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -21,16 +21,11 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
